045183a19b455f0fd9d11187374bb3703e8da2f2
TooTiredOne <kamaliyevkamil@gmail.com>
2020-04-06 21:51:16 +0300

    added gets

--- web/application/views/get_methods.py
+++ web/application/views/get_methods.py
@@ -25,9 +25,37 @@ def get_courses():
     )
 
 
-@bp.route('/get_all_feedbacks', methods=['Get'])
+@bp.route('/get_all_feedbacks', methods=['GET'])
 def get_all_feedbacks():
     data = get_db().get_all_feedback()
     for f in data:
         f['_id'] = str(f['_id'])
+    return jsonify(data)
+
+@bp.route('/get_all_professors', methods=['GET'])
+def get_all_professors():
+    data = get_db().get_all_prof()
+    for p in data:
+        p['_id'] = str(p['_id'])
+    return jsonify(data)
+
+@bp.route('/get_all_students', methods=['GET'])
+def get_all_students():
+    data = get_db().get_all_students()
+    for s in data:
+        s['_id'] = str(s['_id'])
+    return jsonify(data)
+
+@bp.route('/get_all_tas', methods=['GET'])
+def get_all_tas():
+    data = get_db().get_all_ta()
+    for t in data:
+        t['_id'] = str(t['_id'])
+    return jsonify(data)
+
+@bp.route('/get_all_admins', methods=['GET'])
+def get_all_admins():
+    data = get_db().get_all_admin()
+    for a in data:
+        a['_id'] = str(a['_id'])
     return jsonify(data)
