c83fcbafc6d253016d1bed5fe5c2f0dcb6689eef
Alena <alenaz2304@gmail.com>
2020-04-19 21:32:47 +0300

    Add forms

---
+++ vue/src/components/Survey.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Please leave your feedback for {{course}} {{type}}</h1>
+    <div v-if="type==='Course'">
+      <p> How would you rate the course?</p>
+      <p> What materials were useful for understanding the course?</p>
+      <p> What improvements would you make to the course? </p>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Survey",
+    props: ['course', 'type'],
+    data: () => ({
+    }),
+  }
+
+</script>
+
+<style scoped>
+
+</style>
